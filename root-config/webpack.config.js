6b36c9f1e177ff09d6ca623c647abd40068ee8b4
Piotr Hajduga <piotr.hajduga@gmail.com>
2021-04-07 19:53:43 +0200

    Experiments with single-spa

---
+++ root-config/webpack.config.js
@@ -0,0 +1,63 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "firemark";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              // inject CSS to page
+              loader: "style-loader",
+            },
+            {
+              // translates CSS into CommonJS modules
+              loader: "css-loader",
+            },
+            {
+              // Run postcss actions
+              loader: "postcss-loader",
+              options: {
+                // `postcssOptions` is needed for postcss 8.x;
+                // if you use postcss 7.x skip the key
+                postcssOptions: {
+                  // postcss plugins, can be exported to postcss.config.js
+                  plugins: function () {
+                    return [require("autoprefixer")];
+                  },
+                },
+              },
+            },
+            {
+              // compiles Sass to CSS
+              loader: "sass-loader",
+            },
+          ],
+        },
+      ],
+    },
+  });
+};
